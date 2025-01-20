5a5c2778c0ceec06dee9dd0fcab85b7a26a2f663
wonjjee <yoteamo7@naver.com>
2025-01-20 23:28:19 +0900

    feat: setNestApp file 추가

---
+++ setNestApp.ts
@@ -0,0 +1,16 @@
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+export function setNestApp<T extends INestApplication>(app: T): void {
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+}
