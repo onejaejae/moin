e76767a9a102e838ad08ba3662d8cf8e8c3f1466
wonjjee <yoteamo7@naver.com>
2025-01-23 00:12:48 +0900

    feat: added RequestTransferPolicy

---
+++ src/modules/transfer/policy/requestTransfer.policy.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { User } from 'src/entities/user/user.entity';
+import { TransferRepository } from '../repository/transfer.repository';
+import { IdType } from 'src/entities/user/user.interface';
+import { LimitExcessException } from 'src/core/exception/limitExcess.exception';
+
+@Injectable()
+export class RequestTransferPolicy {
+  private readonly DAILY_USD_LIMITS = {
+    [IdType.RegNo]: 1000,
+    [IdType.BusinessNo]: 5000,
+  } as const;
+
+  constructor(private readonly transferRepository: TransferRepository) {}
+
+  async validateDailyTransferLimit(userId: User['id'], idType: User['idType']) {
+    const todayTransfers =
+      await this.transferRepository.findTodayTransfers(userId);
+
+    console.log('todayTransfers---', todayTransfers);
+    const totalUsdAmount = this.calculateTotalUsdAmount(todayTransfers);
+
+    const limit = this.DAILY_USD_LIMITS[idType];
+    if (totalUsdAmount > limit) {
+      throw new LimitExcessException();
+    }
+  }
+
+  private calculateTotalUsdAmount(transfers: any[]): number {
+    return transfers.reduce((acc, curr) => acc + curr.Quote.usdAmount, 0);
+  }
+}
