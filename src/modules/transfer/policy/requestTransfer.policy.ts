a7a6353982a38e0170088bf23929b504a40fc733
wonjjee <yoteamo7@naver.com>
2025-01-23 23:58:49 +0900

    fix: 송금 금액 비교 시, 이상 조건으로 수정

--- src/modules/transfer/policy/requestTransfer.policy.ts
+++ src/modules/transfer/policy/requestTransfer.policy.ts
@@ -3,6 +3,8 @@ import { User } from 'src/entities/user/user.entity';
 import { TransferRepository } from '../repository/transfer.repository';
 import { IdType } from 'src/entities/user/user.interface';
 import { LimitExcessException } from 'src/core/exception/limitExcess.exception';
+import { OmitNotJoinedProps } from 'src/core/database/typeorm/typeorm.interface';
+import { Transfer } from 'src/entities/transfer/transfer.entity';
 
 @Injectable()
 export class RequestTransferPolicy {
@@ -19,12 +21,19 @@ export class RequestTransferPolicy {
     const totalUsdAmount = this.calculateTotalUsdAmount(todayTransfers);
 
     const limit = this.DAILY_USD_LIMITS[idType];
-    if (totalUsdAmount > limit) {
+    if (totalUsdAmount >= limit) {
       throw new LimitExcessException();
     }
   }
 
-  private calculateTotalUsdAmount(transfers: any[]): number {
+  private calculateTotalUsdAmount(
+    transfers: OmitNotJoinedProps<
+      Transfer,
+      {
+        Quote: true;
+      }
+    >[],
+  ): number {
     return transfers.reduce((acc, curr) => acc + curr.Quote.usdAmount, 0);
   }
 }
